4df95839d2f8c5a23f29002c5d8dc08fbfb178ef
ThomasARich <63765428+ThomasARich@users.noreply.github.com>
2020-05-01 11:21:07 -0700

    Update CalcDriver.java

--- src/CalcDriver.java
+++ src/CalcDriver.java
@@ -1,3 +1,10 @@
+/*
+ * CalcDriver : Team Project Deliverable
+ * The SCRUM-at-Home Order
+ * Thomas Rich, Trevor Davis, Mimi Xue
+ * Jessica Lynn, Heavenly Khosa
+ * CSC 131 - 02
+ */
 public class CalcDriver {
    public static void main(String[] args) {
       Calculator c = new Calculator();
