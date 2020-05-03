8af2b22e2488f569506237a3323c9f378ed47f27
ThomasARich <63765428+ThomasARich@users.noreply.github.com>
2020-05-02 22:07:33 -0700

    Add files via upload

---
+++ src/SingletonCalcDriver.java
@@ -0,0 +1,12 @@
+/*
+ * SingletonCalcDriver : Team Project Deliverable
+ * The SCRUM-at-Home Order
+ * Thomas Rich, Trevor Davis, Mimi Xue
+ * Jessica Lynn, Heavenly Khosa
+ * CSC 131 - 02
+ */
+class SingletonCalcDriver {
+   public static void main(String[] args) {
+      SingletonCalculator c = SingletonCalculator.getInstance();
+   }
+}
